1c311d85484062a5b1df3bba2e440ab43527af30
Hung Ngo <bts.frontend.dev@gmail.com>
2024-08-08 00:28:00 +0700

    feat: init folder structure

---
+++ src/components/Button/settings.ts
@@ -0,0 +1,36 @@
+import colors from '@styleguide/theme/colors';
+
+export const backgroundColors = {
+  primary: colors.green,
+  secondary: colors.white,
+  facebook: colors.facebook,
+  google: colors.google,
+  linkedin: colors.linkedin,
+  disabled: colors.lightGray,
+};
+
+export const borderColors = {
+  primary: colors.green,
+  secondary: colors.green,
+  facebook: colors.facebook,
+  google: colors.google,
+  linkedin: colors.linkedin,
+  disabled: colors.lightGray,
+};
+
+export const textColors = {
+  primary: colors.white,
+  secondary: colors.green,
+  facebook: colors.white,
+  google: colors.white,
+  linkedin: colors.white,
+  disabled: colors.black,
+};
+
+export const backgroundHover = {
+  primary: 'rgba(0, 0, 0, .1)',
+  secondary: colors.green,
+  facebook: 'rgba(0, 0, 0, .1)',
+  google: 'rgba(0, 0, 0, .1)',
+  linkedin: 'rgba(0, 0, 0, .1)',
+};
