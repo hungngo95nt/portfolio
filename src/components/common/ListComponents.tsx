eed0f11e1a2ea31a1c9847354df74a28ea288f01
Hung Ngo <bts.frontend.dev@gmail.com>
2024-08-13 00:16:00 +0700

    feat: update inputs, checkboxes

--- src/components/common/ListComponents.tsx
+++ src/components/common/ListComponents.tsx
@@ -1,46 +1,46 @@
 import { FunctionComponent } from 'react';
 
 const ListComponents: FunctionComponent = () => {
-    return (
-        <div className="flex flex-col p-4 gap-2 mt-16 w-1/4">
-            <a
-                href="/components/buttons"
-                className="text-purple-500 hover:underline hover:text-purple-600"
-            >
-                Buttons
-            </a>
-            <a
-                href="/components/inputs"
-                className="text-purple-500 hover:underline hover:text-purple-600"
-            >
-                Inputs
-            </a>
-            <a
-                href="/components/checkboxes"
-                className="text-purple-500 hover:underline hover:text-purple-600"
-            >
-                Checkbox
-            </a>
-            <a
-                href="/components/radios"
-                className="text-purple-500 hover:underline hover:text-purple-600"
-            >
-                Radio
-            </a>
-            <a
-                href="/components/dropdowns"
-                className="text-purple-500 hover:underline hover:text-purple-600"
-            >
-                Dropdown
-            </a>
-            <a
-                href="/components/tooltips"
-                className="text-purple-500 hover:underline hover:text-purple-600"
-            >
-                Tooltip
-            </a>
-        </div>
-    );
+  return (
+    <div className="flex flex-col p-4 gap-2 mt-16 w-1/4">
+      <a
+        href="/components/buttons"
+        className="text-purple-500 hover:underline hover:text-purple-600"
+      >
+        Buttons
+      </a>
+      <a
+        href="/components/inputs"
+        className="text-purple-500 hover:underline hover:text-purple-600"
+      >
+        Inputs
+      </a>
+      <a
+        href="/components/checkboxes"
+        className="text-purple-500 hover:underline hover:text-purple-600"
+      >
+        Checkboxes
+      </a>
+      <a
+        href="/components/radios"
+        className="text-purple-500 hover:underline hover:text-purple-600"
+      >
+        Radios
+      </a>
+      <a
+        href="/components/dropdowns"
+        className="text-purple-500 hover:underline hover:text-purple-600"
+      >
+        Dropdowns
+      </a>
+      <a
+        href="/components/tooltips"
+        className="text-purple-500 hover:underline hover:text-purple-600"
+      >
+        Tooltips
+      </a>
+    </div>
+  );
 };
 
 export default ListComponents;
--- src/components/inputs/SearchInput.tsx
+++ src/components/inputs/SearchInput.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactNode } from 'react';
 
-const OutLinedInput: FunctionComponent<{
+const SearchInput: FunctionComponent<{
   type: string;
   title: string;
   icon?: ReactNode;
@@ -14,21 +14,21 @@ const OutLinedInput: FunctionComponent<{
       >
         {title}
       </label>
-      <div className="relative mt-2 rounded-md shadow-sm justify-between">
+      <div className="relative mt-2 rounded-md shadow-sm">
+        <input
+          id="price"
+          name="price"
+          type={type}
+          placeholder={placeholder}
+          className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20
+           text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+           focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
           <span className="text-gray-500 sm:text-sm">{icon}</span>
-        </div>
-        <div className="mx-1">
-          <input
-            id="price"
-            name="price"
-            type={type}
-            placeholder={placeholder}
-            className="block rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
         </div>
       </div>
     </div>
   );
 };
-export default OutLinedInput;
+export default SearchInput;
